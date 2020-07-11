2dd1339e1261bbf686e19f21a0796cc170324ff4
0-void <muthukumarperiyasamy.mk@gmail.com>
2020-07-11 21:58:41 +0530

    small changes

--- server/controllers/userProductController.js
+++ server/controllers/userProductController.js
@@ -283,7 +283,6 @@ const addFavourite = async (req, res, next) => {
   try {
     await user.save();
   } catch (err) {
-    console.log(err);
     return next(new Error("Adding favourite failed"));
   }
 
