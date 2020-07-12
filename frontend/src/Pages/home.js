328a415479db9982fb46f4778e565816c1640a8e
0-void <muthukumarperiyasamy.mk@gmail.com>
2020-07-12 19:55:38 +0530

    fixed notification size in mobile view and increased button size

--- frontend/src/Pages/home.js
+++ frontend/src/Pages/home.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import "./Home.css";
+import "./home.css";
 import ProductCard from "../components/Home/ProductCard";
 import {
   thunkAddToFav,
