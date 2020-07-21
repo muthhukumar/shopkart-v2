7d10e23bbaba062355f8ddbd70dae3136c92cf60
0-void <muthukumarperiyasamy.mk@gmail.com>
2020-07-21 21:29:53 +0530

    show add to cart and favourite all the time

--- frontend/src/components/Home/ProductCard.js
+++ frontend/src/components/Home/ProductCard.js
@@ -23,44 +23,40 @@ export default function ({
 
   return (
     <div className="home-container">
-      {token && (
-        <button
-          onClick={
-            !fav
-              ? onFavAdd.bind(this, { id, imageUrl, title, price })
-              : onFavRemove.bind(this, id, title)
-          }
-          className={`home-fav ${fav && "home-highlight"}`}
-        >
-          {!fav && <i className="fa fa-heart-o" aria-hidden="true"></i>}
-          {fav && <i className={`fa fa-heart`} aria-hidden="true"></i>}
-        </button>
-      )}
+      <button
+        onClick={
+          !fav
+            ? onFavAdd.bind(this, { id, imageUrl, title, price })
+            : onFavRemove.bind(this, id, title)
+        }
+        className={`home-fav ${fav && "home-highlight"}`}
+      >
+        {!fav && <i className="fa fa-heart-o" aria-hidden="true"></i>}
+        {fav && <i className={`fa fa-heart`} aria-hidden="true"></i>}
+      </button>
       <div className="product-main_container">
         <img src={imageUrl} alt={title} />
         <div className="bottomContainer">
           <h3 className="productName">{title}</h3>
           <div className="price">₹ {price}</div>
-          {token && (
-            <div className="buttonContainer">
-              <button
-                className={`${isInCart && "remove-btn"}`}
-                onClick={
-                  !isInCart
-                    ? onAddToCart.bind(this, {
-                        id,
-                        title,
-                        productCount: 1,
-                        imageUrl,
-                        price,
-                      })
-                    : onProductRemove.bind(this, id, title)
-                }
-              >
-                {!isInCart ? "Add to Cart" : "remove"}
-              </button>
-            </div>
-          )}
+          <div className="buttonContainer">
+            <button
+              className={`${isInCart && "remove-btn"}`}
+              onClick={
+                !isInCart
+                  ? onAddToCart.bind(this, {
+                      id,
+                      title,
+                      productCount: 1,
+                      imageUrl,
+                      price,
+                    })
+                  : onProductRemove.bind(this, id, title)
+              }
+            >
+              {!isInCart ? "Add to Cart" : "remove"}
+            </button>
+          </div>
         </div>
       </div>
     </div>
